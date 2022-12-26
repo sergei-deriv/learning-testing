04deb6c2f557ef0e15dd207dc94619cd4a7871a2
Sergei Baranovski <sergei@deriv.com>
2022-12-26 18:08:06 +0300

    feat: add some tests for Counter

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Counter } from './Counter';
+// import userEvent from '@testing-library/user-event';
 
-it('counter must work', () => {
-  render(<Counter />);
-  const count = screen.queryByRole('heading') as HTMLHeadingElement;
-  expect(count.textContent).toBe('Count: 0');
+describe('init with def value 0', () => {
+  const setup = () => render(<Counter />);
+
+  //   beforeEach(() => {});
+
+  it('def value 0', () => {
+    setup();
+    const count = screen.queryByRole('heading') as HTMLHeadingElement;
+    expect(count).toBeInTheDocument();
+    expect(count.textContent).toBe('Count: 0');
+  });
+
+  it('def value 0, then counter must be 1', () => {
+    setup();
+    const count = screen.queryByRole('heading') as HTMLHeadingElement;
+    expect(count).toBeInTheDocument();
+    expect(count.textContent).toBe('Count: 0');
+
+    fireEvent.click(screen.getByRole('button', { name: /inc/ }));
+    expect(count.textContent).toBe('Count: 1');
+  });
+
+  it('def value 0, then counter must be -1', () => {
+    setup();
+    const count = screen.queryByRole('heading') as HTMLHeadingElement;
+    expect(count).toBeInTheDocument();
+    expect(count.textContent).toBe('Count: 0');
+
+    fireEvent.click(screen.getByRole('button', { name: /dec/ }));
+    expect(count.textContent).toBe('Count: -1');
+  });
 });

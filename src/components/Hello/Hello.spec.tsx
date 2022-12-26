643751aabe12672a7d2dc81b1dd55c97613a4cb2
Sergei Baranovski <sergei@deriv.com>
2022-12-26 15:56:40 +0300

    feat: create Hello component and first test for it

---
+++ src/components/Hello/Hello.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Hello } from './Hello';
+
+test('renders "Hello!"', () => {
+  render(<Hello />);
+  const myElement = screen.getByText('Hello!');
+  expect(myElement).toBeInTheDocument();
+});

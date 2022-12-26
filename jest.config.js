effcc0f4d989dbb7cab7ceae7b6987ea0a7362b7
Sergei Baranovski <sergei@deriv.com>
2022-12-26 15:56:23 +0300

    feat: add some settings and install jest-environment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.jest.json'
+    }
+  }
 };

c6642919ca345527dcaed55a688592a1c5c4236c
Sergei Baranovski <sergei@deriv.com>
2022-12-27 10:25:30 +0300

    feat: change name of describe for counter test

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -1,8 +1,8 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Counter } from './Counter';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 
-describe('init with def value 0', () => {
+describe('counter test', () => {
   const setup = () => render(<Counter />);
 
   //   beforeEach(() => {});
@@ -24,13 +24,14 @@ describe('init with def value 0', () => {
     expect(count.textContent).toBe('Count: 1');
   });
 
-  it('def value 0, then counter must be -1', () => {
+  it('def value 0, then counter must be -1', async () => {
     setup();
     const count = screen.queryByRole('heading') as HTMLHeadingElement;
     expect(count).toBeInTheDocument();
     expect(count.textContent).toBe('Count: 0');
 
-    fireEvent.click(screen.getByRole('button', { name: /dec/ }));
+    // fireEvent.click(screen.getByRole('button', { name: /dec/ }));
+    await userEvent.click(screen.getByRole('button', { name: /dec/ }));
     expect(count.textContent).toBe('Count: -1');
   });
 });
